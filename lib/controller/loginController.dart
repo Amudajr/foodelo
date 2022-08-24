a439923e0c149918c2e3655fe5e2e00c5f085d37
@classicallow26 <@classicallow26@gmail.com>
2022-08-24 09:45:43 +0100

    fir commit

---
+++ lib/controller/loginController.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:foodello/model/loginModel.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  TextEditingController loginEditingController = TextEditingController();
+
+  void login() {}
+}
